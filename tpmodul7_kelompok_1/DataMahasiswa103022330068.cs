b34eeb5373ae5e1d28af529af49bee4c154ffcdf
fadhlidzaki <fadhlidzaki120@gmail.com>
2025-04-06 19:13:52 +0700

    menambahkan class DataMahasiswa103022330068

---
+++ tpmodul7_kelompok_1/DataMahasiswa103022330068.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using static DataMahasiswa103022330068;
+
+class DataMahasiswa103022330068
+{
+    public class Nama
+    {
+        public String depan;
+        public String belakang;
+    }
+    public class Mahasiswa103022330068
+    {
+        public Nama nama;
+        public long nim;
+        public String fakultas;
+    }
+
+    public  void ReadJSON()
+    {
+        var dataJson = File.ReadAllText("../../../tp7_1_103022330068.json");
+
+        var mahasiswa = JsonSerializer.Deserialize<Mahasiswa103022330068>(dataJson, new JsonSerializerOptions { IncludeFields = true });
+
+        Console.WriteLine("Nama {0} {1} dengan NIM {2} dari fakultas {3} ", mahasiswa.nama.depan, mahasiswa.nama.belakang, mahasiswa.nim, mahasiswa.fakultas);
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa103022330068 data = new DataMahasiswa103022330068();
+        data.ReadJSON();
+    }
+}
